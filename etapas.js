be960c2e3c237ed8ee51e716c5447b701a9f7089
Cleiton Edgard Ozildio de Souza <cleiton.souza@apoioprodesp.sp.gov.br>
2022-06-26 16:21:39 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Woody',
+                partido: 'Toy Story',
+                fotos: [
+                    {url:'avatar-3.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Buzz',
+                partido: 'Toy Story',
+                fotos: [
+                    {url:'avatar-4.png', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '99',
+                nome: 'Sven',
+                partido: 'Frozen',
+                vice: 'kristoff',
+                fotos: [
+                    {url:'avatar-5.png', legenda: 'Prefeito'},
+                    {url:'avatar-6.png', legenda: 'Vice-Prefeito', small: true} 
+                ]
+            },
+            {
+                numero: '54',
+                nome: 'Vanellope',
+                partido: 'Disney',
+                vice: 'Ralph',
+                fotos: [
+                    {url:'avatar-5.png', legenda: 'Prefeito'},
+                    {url:'avatar-6.png', legenda: 'Vice-Prefeito', small: true} 
+                ]
+            }
+        ] 
+    }
+];
---
+++ script.js
@@ -0,0 +1,127 @@
+let seuVotoPpara = document.querySelector('.d-1-1 span')
+let cargo = document.querySelector('.d-1-2 span')
+let descicao = document.querySelector('.d-1-4')
+let aviso = document.querySelector('.d-2')
+let lateral = document.querySelector('.d-1-right')
+let numeros = document.querySelector('.d-1-3')
+
+let etapaAtual = 0
+let numero = ''
+let votoBranco = false
+
+function comecarEtapa(){
+    let etapa = etapas[etapaAtual]
+
+    let numeroHTML = ''
+    numero = ''
+    votoBranco = false
+
+
+    for(i = 0; i<etapa.numeros; i++){
+        if(i === 0){
+            numeroHTML += '<div class="numero pisca "></div>'
+        } else {
+            numeroHTML += '<div class="numero "></div>'
+        }
+    }
+
+    seuVotoPpara.style.display = 'none'
+    cargo.innerHTML = etapa.titulo
+    descicao.innerHTML = ''
+    aviso.style.display = 'none'
+    lateral.innerHTML = ''
+    numeros.innerHTML = numeroHTML 
+
+}
+
+function atualizaInterface(){
+    let etapa = etapas[etapaAtual]
+    let candidato = etapa.candidatos.filter((item) => {
+        if(item.numero === numero){
+            return true
+        } else {
+            return false
+        }
+    })
+
+    if(candidato.length > 0) {
+        candidato = candidato[0]
+        seuVotoPpara.style.display = 'block'
+        aviso.style.display = 'block'
+        descicao.innerHTML = `Nome: ${candidato.nome}<br/> Partido: ${candidato.partido}`
+
+        let fotosHtml = ''
+        for(let i in candidato.fotos){
+            if(candidato.fotos[i].small){
+                fotosHtml += `<div class="d-1-image small"><img src="img/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`
+            } else {
+                fotosHtml += `<div class="d-1-image"><img src="img/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`
+            }
+            
+        }
+        lateral.innerHTML = fotosHtml
+    } else {
+        seuVotoPpara.style.display = 'block'
+        aviso.style.display = 'block'
+        descicao.innerHTML = '<div class="aviso--grande pisca">VOTO NULLO</div>'
+    } 
+
+    console.log('Candidato', candidato)
+    
+}
+
+function clicou(n){
+    let elNumero = document.querySelector('.numero.pisca')
+    
+    if(elNumero !== null){
+        elNumero.innerHTML = n
+        numero = `${numero}${n}`
+
+        elNumero.classList.remove('pisca')
+        if(elNumero.nextElementSibling !== null){
+            elNumero.nextElementSibling.classList.add('pisca')
+        } else {
+            atualizaInterface()
+        }
+
+    }
+}
+
+function branco(){
+    if(numero === ''){
+        votoBranco = true
+
+        seuVotoPpara.style.display = 'block'
+        aviso.style.display = 'block'
+        numeros.innerHTML = ''
+        descicao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>'
+    }
+}
+
+function corrigi(){
+    comecarEtapa()
+}
+
+function confirma(){
+    let etapa = etapas[etapaAtual]
+
+    let votoConfirmado = false
+    if(votoBranco === true){
+         votoConfirmado = true
+
+    } else if(numero.length === etapa.numeros){
+         votoConfirmado = true
+
+    }
+
+    if(votoConfirmado){
+        etapaAtual++
+        if(etapas[etapaAtual] !== undefined){
+            comecarEtapa()
+        } else {
+           document.querySelector('.tela').innerHTML = '<div class="aviso--g pisca">FIM!!</div>'
+        }
+    }
+}
+
+comecarEtapa()
